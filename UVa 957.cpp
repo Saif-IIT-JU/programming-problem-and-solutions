90b513be350aa173f49b7ea898e7733d4163cba9
Saif-IIT-JU <57219416+Saif-IIT-JU@users.noreply.github.com>
2019-11-26 15:02:05 +0600

    Create UVa 957.cpp

---
+++ UVa 957.cpp
@@ -0,0 +1,79 @@
+/*
+
+    author : s@if
+
+*/
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define 		NIL 			-1
+#define		 	fi	 		    first
+#define 		sec 			second
+#define 		MAX 			INT_MAX
+#define			INF 			99999999
+#define 		ll 			    long long
+#define 		PI 			    acos(-1.0)
+#define			MOD 			1000000007
+#define 		PLL 			pair<ll,ll>
+#define 		PII			    pair<int,int>
+#define 		ull 			unsigned long long
+#define 		For(i,a,b) 		for(int i=a;i<=(int)b;i++)
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+            tree_order_statistics_node_update> new_data_set;
+
+//*find_by_order(k)  gives the kth element;
+//order_of_key(item)   gives the index(number of element strictly less than item) of item;
+
+
+inline int in() {int x; scanf("%d", &x); return x; }
+bool Check(int N , int pos)	{	return (bool) (N & (1<<pos));}
+int Set(int N, int pos) {	return N = N | (1<<pos);}
+
+int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};
+int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};
+int hr[]={-2,-2,-1,+1,+2,+2,-1,+1};
+int hc[]={+1,-1,+2,+2,-1,+1,-2,-2};
+int dx[]={+1,-1,+0,+0};
+int dy[]={+0,+0,+1,-1};
+
+const int MAXN = (int)1e5+9;
+
+int main()
+{
+/*	freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);  */
+
+    int i,j,k,l,m,n,p,q,x,y,u,v,r,tc,t;
+
+    while(scanf("%d", &y)==1)
+    {
+        scanf("%d", &n); int ara[n+9];
+        For(i,0,n-1) ara[i] = in();
+
+        int ans = 0;
+
+        for(i=0; i<n; i++)
+        {
+            int ub = upper_bound(ara, ara+n, ara[i]+y-1) - ara;
+
+            if(ub-i>ans)
+            {
+                ans = ub-i;
+                l = ara[i]; r = ara[ub-1];
+            }
+        }
+
+        printf("%d %d %d\n", ans, l, r);
+    }
+
+    return 0;
+}
+
+// read the question correctly (is y a vowel? what are the exact constraints?)
+// look out for SPECIAL CASES (n=1?) and overflow (ll vs int?)
